dcb6761e8b0796769ed00ee19ce851d4b533ff1e
seshuk <k.seshu@live.in>
2016-06-10 13:36:25 +1000

    code clean-up

--- xFitbitClient/xFitbitClient/xFitbitClient/Views/ProfileView.xaml.cs
+++ xFitbitClient/xFitbitClient/xFitbitClient/Views/ProfileView.xaml.cs
@@ -30,8 +30,6 @@ namespace xFitbitClient
 
         public async void BtnGetProfile_Clicked(object sender, EventArgs args)
         {
-            //webView.Source = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=227M43&redirectUri=http://seshuk.com/fitbitdataservice/&scope=activity%20location%20profile&expires_in=604800";
-            //aState = AuthState.authRequest;
             profileViewLayout.Children.Add(busyIndicator);
             busyIndicator.IsRunning = true;
             FitbitProfileHelper helper = new FitbitProfileHelper();
@@ -43,22 +41,6 @@ namespace xFitbitClient
 
         void webOnNavigating(object sender, WebNavigatingEventArgs e)
         {
-            if (e.Url.ToString().StartsWith("https://www.fitbit.com/login") && aState != AuthState.login)
-                aState = AuthState.login;
-            if(e.Url.ToString().StartsWith("http://seshuk.com/fitbit"))
-            {
-                e.Cancel = true;
-                // Auth completed. Get the token and start the requests...
-                webView.IsVisible = false;
-                
-            }
-            
-            if(e.Url.ToString().StartsWith("https://www.fitbit.com/oauth2/") && 
-                aState == AuthState.login && 
-                !e.Url.ToString().Contains("http://seshuk.com/fitbitdataservice"))
-                webView.Source = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=227M43&redirectUri=http://seshuk.com/fitbitdataservice/&scope=activity%20location%20profile&expires_in=604800";
-
-            System.Diagnostics.Debug.WriteLine(e.Url);
         }
     }
 }
